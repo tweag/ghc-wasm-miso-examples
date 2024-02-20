4270857ad91eb9b27e199adb177d701121b411eb
Cheng Shao <terrorjack@type.dance>
2024-02-20 20:03:45 +0000

    Bump flakes and use same nixpkgs from ghc-wasm-meta

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     ghc-wasm-meta.url = "gitlab:ghc/ghc-wasm-meta?host=gitlab.haskell.org";
   };
-  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = inputs.nixpkgs.legacyPackages.${system};
+  outputs = inputs: inputs.ghc-wasm-meta.inputs.flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = inputs.ghc-wasm-meta.inputs.nixpkgs.legacyPackages.${system};
     in
     {
       devShells.default = pkgs.mkShell {
