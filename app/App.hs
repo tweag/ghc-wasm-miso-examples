f04e4bf03a42c0e88326169d2a6e0bf17731cf10
Cheng Shao <terrorjack@type.dance>
2024-03-11 13:51:01 +0000

    Avoid using env var at runtime

--- app/App.hs
+++ app/App.hs
@@ -1,16 +1,15 @@
 module App (start) where
 
-import Control.Monad.IO.Class
+import GHC.Wasm.Prim
 import Language.Javascript.JSaddle (JSM)
 import SimpleCounter qualified
 import Snake qualified
-import System.Environment
 import TodoMVC qualified
 import TwoZeroFourEight qualified
 
-start :: JSM ()
-start =
-  liftIO (getEnv "EXAMPLE") >>= \case
+start :: JSString -> JSM ()
+start e =
+  case fromJSString e of
     "simplecounter" -> SimpleCounter.start
     "snake" -> Snake.start
     "todomvc" -> TodoMVC.start
--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
-module Main (main) where
+module MyMain (main) where
 
 import App (start)
+import GHC.Wasm.Prim
 import Language.Javascript.JSaddle.Wasm qualified as JSaddle.Wasm
 
-foreign export javascript "hs_start" main :: IO ()
+foreign export javascript "hs_start" main :: JSString -> IO ()
 
-main :: IO ()
-main = JSaddle.Wasm.run start
+main :: JSString -> IO ()
+main e = JSaddle.Wasm.run $ start e
