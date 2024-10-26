ee587a4a2ea14d1c297d8fab7d36a05fd27c7647
Alexander Esgen <alexander.esgen@tweag.io>
2024-10-26 17:52:55 +0200

    Add reflex-todomvc

--- app/App.hs
+++ app/App.hs
@@ -2,6 +2,7 @@ module App (start) where
 
 import GHC.Wasm.Prim
 import Language.Javascript.JSaddle (JSM)
+import Reflex.TodoMVC qualified
 import SimpleCounter qualified
 import Snake qualified
 import TodoMVC qualified
@@ -16,4 +17,5 @@ start e =
     "todomvc" -> TodoMVC.start
     "xhr" -> XHR.start
     "2048" -> TwoZeroFourEight.start
+    "reflex-todomvc" -> Reflex.TodoMVC.main
     _ -> fail "unknown example"
