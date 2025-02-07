2eb76f1da06c18a0dd972282e5a7fad5940fce88
Cheng Shao <terrorjack@type.dance>
2025-02-07 20:42:06 +0000

    chore: use ghc 9.12

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     {
       devShells.default = pkgs.mkShell {
         packages = [
-          inputs.ghc-wasm-meta.packages.${system}.all_9_10
+          inputs.ghc-wasm-meta.packages.${system}.all_9_12
           pkgs.dart-sass
         ];
       };
